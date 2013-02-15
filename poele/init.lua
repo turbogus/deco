392c49d4d9498f5ba091e44be9b84354f0c35043
turbogus <hooover@hotmail.fr>
2013-02-15 17:19:48 +0100

    ajout poele a bois
    ajout du mode poele a bois
    Permet de crafter un poele a bois, qui a terme redonnera de la vie quand
    on se trouve a proximité si il est allumé. De la fumée s'echappera du
    haut du tubage quand le poele fonctionne ( fumée toxique si on est
    dedans )
    
    ok :
    
    -poele
    -tubage
    
    craft poele
    
    X=rien
    S = steel_ingot
    G = glass
    
    poele :
    S S S
    S G S
    S S S
    
    tube
    
    S X S
    S X S
    S X S

---
+++ poele/init.lua
@@ -0,0 +1,253 @@
+--POELE
+
+--mod créé par turbogus
+
+--graphisme et code sous licence gpl v2 ou superieur
+
+--mod qui vous permet de crafter un poele a bois pour décorer votre maison dans minetest
+
+--INDEX
+--DECLARATION DES BLOCS
+--PARAMETRES DES BLOCS
+
+
+--INDEX :
+
+--poele:poele
+--poele:tube
+--poele:fumee
+--poele:allume_feu
+--poele:buche
+--poele:cendre
+
+
+
+--DEBUT
+
+--DECLARATION DES BLOCS
+
+--poele:poele
+minetest.register_craft ({
+	output = "poele:poele",
+	recipe = {
+		{"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
+		{"default:steel_ingot","default:glass","default:steel_ingot"},
+		{"default:steel_ingot","default:steel_ingot","default:steel_ingot"},
+	}
+})
+	
+--poele:tube
+minetest.register_craft ({
+	output = "poele:tube",
+	recipe = {
+		{"default:steel_ingot","","default:steel_ingot"},
+		{"default:steel_ingot","","default:steel_ingot"},
+		{"default:steel_ingot","","default:steel_ingot"},
+	}
+})
+
+--poele:allume_feu
+
+
+
+--PARAMETRES DES BLOCS
+
+--poele:poele
+minetest.register_node("poele:poele", {
+	description = "Poele a bois",
+	tiles = {"poele.png", "poele.png", "poele.png",
+		"poele.png", "poele.png", "poele_devant_off.png"},
+	paramtype2 = "facedir",
+	groups = {cracky=2},
+	legacy_facedir_simple = true,
+	--sounds = default.node_sound_stone_defaults(),
+	on_construct = function(pos)
+		local meta = minetest.env:get_meta(pos)
+		meta:set_string("formspec", default.furnace_inactive_formspec)
+		meta:set_string("infotext", "Furnace")
+		local inv = meta:get_inventory()
+		inv:set_size("fuel", 1)
+		inv:set_size("src", 1)
+		inv:set_size("dst", 4)
+	end,
+	can_dig = function(pos,player)
+		local meta = minetest.env:get_meta(pos);
+		local inv = meta:get_inventory()
+		if not inv:is_empty("fuel") then
+			return false
+		elseif not inv:is_empty("dst") then
+			return false
+		elseif not inv:is_empty("src") then
+			return false
+		end
+		return true
+	end,
+})
+
+minetest.register_node("poele:poele_on", {
+	description = "Poele a bois",
+	tiles = {"poele.png", "poele.png", "poele.png",
+		"poele.png", "poele.png", "poele_devant_on.png"},
+	paramtype2 = "facedir",
+	paramtype2 = "facedir",
+	light_source = 16,
+	drop = "poele:poele",
+	groups = {cracky=2, not_in_creative_inventory=1},
+	legacy_facedir_simple = true,
+	--sounds = default.node_sound_stone_defaults(),
+	on_construct = function(pos)
+		local meta = minetest.env:get_meta(pos)
+		meta:set_string("formspec", default.furnace_inactive_formspec)
+		meta:set_string("infotext", "Furnace");
+		local inv = meta:get_inventory()
+		inv:set_size("fuel", 1)
+		inv:set_size("src", 1)
+		inv:set_size("dst", 4)
+	end,
+	can_dig = function(pos,player)
+		local meta = minetest.env:get_meta(pos);
+		local inv = meta:get_inventory()
+		if not inv:is_empty("fuel") then
+			return false
+		elseif not inv:is_empty("dst") then
+			return false
+		elseif not inv:is_empty("src") then
+			return false
+		end
+		return true
+	end,
+})
+
+function hacky_swap_node(pos,name)
+	local node = minetest.env:get_node(pos)
+	local meta = minetest.env:get_meta(pos)
+	local meta0 = meta:to_table()
+	if node.name == name then
+		return
+	end
+	node.name = name
+	local meta0 = meta:to_table()
+	minetest.env:set_node(pos,node)
+	meta = minetest.env:get_meta(pos)
+	meta:from_table(meta0)
+end
+
+minetest.register_abm({
+	nodenames = {"poele:poele","poele:poele_on"},
+	interval = 1.0,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local meta = minetest.env:get_meta(pos)
+		for i, name in ipairs({
+				"fuel_totaltime",
+				"fuel_time",
+				"src_totaltime",
+				"src_time"
+		}) do
+			if meta:get_string(name) == "" then
+				meta:set_float(name, 0.0)
+			end
+		end
+
+		local inv = meta:get_inventory()
+
+		local srclist = inv:get_list("src")
+		local cooked = nil
+		
+		if srclist then
+			cooked = minetest.get_craft_result({method = "cooking", width = 1, items = srclist})
+		end
+		
+		local was_active = false
+		
+		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
+			was_active = true
+			meta:set_float("fuel_time", meta:get_float("fuel_time") + 1)
+			meta:set_float("src_time", meta:get_float("src_time") + 1)
+			if cooked and cooked.item and meta:get_float("src_time") >= cooked.time then
+				-- check if there's room for output in "dst" list
+				if inv:room_for_item("dst",cooked.item) then
+					-- Put result in "dst" list
+					inv:add_item("dst", cooked.item)
+					-- take stuff from "src" list
+					srcstack = inv:get_stack("src", 1)
+					srcstack:take_item()
+					inv:set_stack("src", 1, srcstack)
+				else
+					print("Could not insert '"..cooked.item:to_string().."'")
+				end
+				meta:set_string("src_time", 0)
+			end
+		end
+		
+		if meta:get_float("fuel_time") < meta:get_float("fuel_totaltime") then
+			local percent = math.floor(meta:get_float("fuel_time") /
+					meta:get_float("fuel_totaltime") * 100)
+			meta:set_string("infotext","Furnace active: "..percent.."%")
+			hacky_swap_node(pos,"poele:poele_on") --*
+			meta:set_string("formspec",
+				"size[8,9]"..
+				"image[2,2;1,1;default_furnace_fire_bg.png^[lowpart:"..
+						(100-percent)..":default_furnace_fire_fg.png]"..
+				"list[current_name;fuel;2,3;1,1;]"..
+				"list[current_name;src;2,1;1,1;]"..
+				"list[current_name;dst;5,1;2,2;]"..
+				"list[current_player;main;0,5;8,4;]")
+			return
+		end
+
+		local fuel = nil
+		local cooked = nil
+		local fuellist = inv:get_list("fuel")
+		local srclist = inv:get_list("src")
+		
+		if srclist then
+			cooked = minetest.get_craft_result({method = "cooking", width = 1, items = srclist})
+		end
+		if fuellist then
+			fuel = minetest.get_craft_result({method = "fuel", width = 1, items = fuellist})
+		end
+
+		if fuel.time <= 0 then
+			meta:set_string("infotext","Furnace out of fuel")
+			hacky_swap_node(pos,"poele:poele")
+			meta:set_string("formspec", default.furnace_inactive_formspec)
+			return
+		end
+
+		if cooked.item:is_empty() then
+			if was_active then
+				meta:set_string("infotext","Furnace is empty")
+				hacky_swap_node(pos,"poele:poele")
+				meta:set_string("formspec", default.furnace_inactive_formspec)
+			end
+			return
+		end
+
+		meta:set_string("fuel_totaltime", fuel.time)
+		meta:set_string("fuel_time", 0)
+		
+		local stack = inv:get_stack("fuel", 1)
+		stack:take_item()
+		inv:set_stack("fuel", 1, stack)
+	end,
+})
+
+--poele:tube
+minetest.register_node("poele:tube", {
+	description = "Tubage pour poele a bois",
+	drawtype = "fencelike",
+	tiles = {"tube.png"},
+	inventory_image = "tube_image_inventaire.png",
+	wield_image = "tube_image_inventaire.png",
+	paramtype = "light",
+	is_ground_content = true,
+	selection_box = {
+		type = "fixed",
+		fixed = {-1/7, -1/2, -1/7, 1/7, 1/2, 1/7},
+	},
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2},
+	
+})
+
+--FIN
